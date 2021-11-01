7b2f028ede5fc625fa3f8d7f775815e9d0271c86
Nicolas Rousset <nrousset@gmail.com>
2021-11-01 15:47:20 +0100

    WiP

---
+++ src/test/java/wcsdata/xmen/repository/CerebookUserDAOTest.java
@@ -0,0 +1,12 @@
+package wcsdata.xmen.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CerebookUserDAOTest {
+    @Test
+    public void findByName() {
+        
+    }
+}
