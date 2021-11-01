6bca6d48e079a1d45094ff4e2fffa94fca0e340b
Nicolas Rousset <nrousset@gmail.com>
2021-11-01 14:31:32 +0100

    Adding docker files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM heroku/heroku:20
+
+RUN apt-get update && apt-get install -y maven
+RUN mkdir /app
+
+WORKDIR /app
+
+ADD pom.xml /app
+RUN mvn dependency:resolve 
+
+ADD . /app
+
+RUN mvn install
+
+CMD mvn spring-boot:run
---
+++ scripts/run_on_docker.sh
@@ -0,0 +1,8 @@
+RETRIES=30
+
+until psql $FULL_DATABASE_URL -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
+
+mvn spring-boot:run
