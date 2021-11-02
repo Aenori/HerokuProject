a6ab48b53bc12c3ac10fe09c0671647d2c89f85f
Nicolas Rousset <nrousset@gmail.com>
2021-11-02 13:27:11 +0100

    Correcting tests

--- src/test/java/wcsdata/xmen/controller/SampleControllerTest.java
+++ src/test/java/wcsdata/xmen/controller/SampleControllerTest.java
@@ -3,29 +3,47 @@ package wcsdata.xmen.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import wcsdata.xmen.Main;
+import wcsdata.xmen.repository.CerebookUserDAO;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import javax.sql.DataSource;
 
 @SpringBootTest()
 @ActiveProfiles("test")
 @Transactional
-@AutoConfigureWebTestClient
+@AutoConfigureMockMvc
 class SampleControllerTest {
     @Autowired
-    private WebTestClient webClient;
+    private MockMvc mvc;
 
     @Test
     void index() {
-        this.webClient.get().uri("/").exchange().expectStatus().isOk()
-                .expectBody(String.class);
     }
 
     @Test
-    void getAllCerebookUser() {
+    void getAllCerebookUser() throws Exception {
+        mvc.perform(get("/cerebookUsers")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name", is("Wolverine")));
     }
 
     @Test
