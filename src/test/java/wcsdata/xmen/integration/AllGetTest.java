4f01a74047640591849d8ebe52c8db2920782dcc
Nicolas Rousset <nrousset@gmail.com>
2021-11-26 12:20:52 +0100

    Testing dirtyContext in github

--- src/test/java/wcsdata/xmen/integration/AllGetTest.java
+++ src/test/java/wcsdata/xmen/integration/AllGetTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.util.Pair;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @Transactional
 @AutoConfigureMockMvc(addFilters = false)
+@DirtiesContext
 public class AllGetTest {
     private int failureCount;
 
